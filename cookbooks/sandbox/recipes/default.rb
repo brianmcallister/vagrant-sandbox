3101da32331476af794bd2a5937c699180791537
Brian McAllister <brian@brianmcallister.com>
2014-02-23 16:10:11 -0500

    Install the Berkshelf gem.

--- cookbooks/sandbox/recipes/default.rb
+++ cookbooks/sandbox/recipes/default.rb
@@ -1,4 +1,20 @@
+# Update apt.
+execute 'apt-get update' do
+  command 'apt-get update'
+  action :run
+end
+
+%w{build-essential ruby1.9.3}.each do |pkg|
+  package pkg
+end
+
+directory '/var/lib/gems' do
+  owner 'vagrant'
+end
+
+# Install the Berkshelf gem to manage recipes.
 gem_package 'berkshelf' do
+  gem_binary 'gem'
   version '3.0.0.beta7'
   action :install
 end
